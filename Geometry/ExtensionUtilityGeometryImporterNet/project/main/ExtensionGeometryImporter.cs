5c95ee10d03b427d2f2fb4d456eea8de541868aa
igrek <igrek@encycam.com>
2024-09-19 09:47:52 +0300

    Examples: - Geometry -> GeometryImporter - ProjectMachine -> ProjectMachineInfo

---
+++ Geometry/ExtensionUtilityGeometryImporterNet/project/main/ExtensionGeometryImporter.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.InteropServices;
+using CAMAPI.Application;
+using CAMAPI.Extensions;
+using CAMAPI.GeomImporter;
+using CAMAPI.Project;
+using CAMAPI.ResultStatus;
+
+namespace ExtensionUtilityGeometryImporterNet;
+
+/// <summary>
+/// Utility to import geometry from Milling_25D\Part1.igs into the active project
+/// </summary>
+public class ExtensionGeometryImporter: IExtension, IExtensionUtility
+{
+    /// <inheritdoc />
+    public IExtensionInfo? Info { get; set; }
+
+    /// <inheritdoc />
+    public void Run(IExtensionUtilityContext context, out TResultStatus resultStatus)
+    {
+        resultStatus = default;
+        ICamApiProject? activeProject = null;
+        ICAMAPIGeometryImporter? importer = null;
+        try
+        {
+            // active project
+            activeProject = context.CamApplication.GetActiveProject(out resultStatus);
+            if (resultStatus.Code == TResultStatusCode.rsError)
+                throw new Exception("Can't get active project: " + resultStatus.Description);
+            if (activeProject == null)
+                throw new Exception("Active project is not found");
+            
+            // geometry importer
+            importer = activeProject.GeomImporter;
+            var modelFileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments),
+                    @"ENCY\Version 1\Models\Milling_25D\Part1.igs");
+            resultStatus = importer.ImportFile(modelFileName, @"", true);
+            if (resultStatus.Code == TResultStatusCode.rsError)
+                throw new Exception("Can't import file: " + resultStatus.Description);
+        } catch (Exception e) {
+            resultStatus.Code = TResultStatusCode.rsError;
+            resultStatus.Description = e.Message;
+        } finally {
+            // mandatory release of COM objects
+            if (importer != null)
+                Marshal.ReleaseComObject(importer);
+            if (activeProject != null)
+                Marshal.ReleaseComObject(activeProject);
+        }
+    }
+}
---
+++ ProjectMachine/ExtensionUtilityProjectMachineInfoNet/project/main/ExtensionProjectMachineInfo.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using CAMAPI.Application;
+using CAMAPI.Extensions;
+using CAMAPI.Machine;
+using CAMAPI.Project;
+using CAMAPI.ResultStatus;
+
+namespace ExtensionUtilityProjectMachineInfoNet;
+
+/// <summary>
+/// Utility to import geometry from Milling_25D\Part1.igs into the active project
+/// </summary>
+public class ExtensionProjectMachineInfo: IExtension, IExtensionUtility
+{
+    /// <inheritdoc />
+    public IExtensionInfo? Info { get; set; }
+
+    /// <inheritdoc />
+    public void Run(IExtensionUtilityContext context, out TResultStatus resultStatus)
+    {
+        resultStatus = default;
+        ICamApiProject? activeProject = null;
+        ICamApiMachineInfo? machineInfo = null;
+        try
+        {
+            // active project
+            activeProject = context.CamApplication.GetActiveProject(out resultStatus);
+            if (resultStatus.Code == TResultStatusCode.rsError)
+                throw new Exception("Can't get active project: " + resultStatus.Description);
+            if (activeProject == null)
+                throw new Exception("Active project is not found");
+            
+            // machine information
+            machineInfo = activeProject.MachineInformation;
+            var tmpFileName = Path.GetTempFileName();
+            File.WriteAllText(tmpFileName,
+                "Current project: " + activeProject.FilePath + Environment.NewLine +
+                "Machine caption: " + machineInfo.MachineCaption + Environment.NewLine +
+                "Machine GUID: " + machineInfo.GUID + Environment.NewLine +
+                "Machine file path: " + machineInfo.SchemaFilePath + Environment.NewLine +
+                "Machine xml node name: " + machineInfo.XMLNodeName + Environment.NewLine
+            );
+            Process.Start("notepad.exe", tmpFileName);
+        } catch (Exception e) {
+            resultStatus.Code = TResultStatusCode.rsError;
+            resultStatus.Description = e.Message;
+        } finally {
+            // mandatory release of COM objects
+            if (machineInfo != null)
+                Marshal.ReleaseComObject(machineInfo);
+            if (activeProject != null)
+                Marshal.ReleaseComObject(activeProject);
+        }
+    }
+}
