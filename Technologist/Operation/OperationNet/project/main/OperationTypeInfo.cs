d88e3d732545b31a83bc2c4106c8be977ffe2712
sabzero0 <renal@encycam.io>
2025-10-29 15:51:52 +0300

    Example with creating operation - done
    More information about operation in list
    
    Example with creating operation - done"
    
    More information about operation in list

---
+++ Technologist/Operation/OperationNet/project/main/OperationTypeInfo.cs
@@ -0,0 +1,32 @@
+
+namespace ExtensionOperationsNet
+{
+    /// <summary>
+    /// A class for operation parameters
+    /// </summary>
+    public partial class OperationTypeInfo
+    {
+        /// <summary>
+        /// Parameter for operation`s Id
+        /// </summary>
+        public string Id { get; set; } = string.Empty;
+        /// <summary>
+        /// Parameter for operation`s caption
+        /// </summary> 
+        public string Caption { get; set; } = string.Empty;
+        /// <summary>
+        /// Parameter for operation`s XMLFilePath
+        /// </summary>
+        public string FilePath { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public OperationTypeInfo(string id, string caption, string filePath)
+        {
+            Id = id;
+            Caption = caption;
+            FilePath = filePath;
+        }
+    }
+};
