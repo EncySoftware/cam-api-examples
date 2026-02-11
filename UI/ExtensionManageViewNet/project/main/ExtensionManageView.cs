ebb6da291ee0b624bd6f8e262634431e1a808964
igrek <igrek@encycam.com>
2026-02-11 14:11:41 +0200

    New example - working with view port and view cube

---
+++ UI/ExtensionManageViewNet/project/main/ExtensionManageView.cs
@@ -0,0 +1,89 @@
+﻿using System.Windows;
+using CAMAPI.Application;
+using CAMAPI.DotnetHelper;
+using CAMAPI.Extensions;
+using CAMAPI.ResultStatus;
+using CAMAPI.ViewPort;
+
+namespace ExtensionManageViewNet;
+
+/// <summary>
+/// Utility to manage the main window view
+/// </summary>
+public class ExtensionManageView: IExtension, IExtensionUtility, IExtensionLazyUnloadable
+{
+    private long _mainWindowHandle;
+    private bool _canUnload;
+
+    /// <inheritdoc />
+    public IExtensionInfo? Info { get; set; }
+    
+    private void ShowForm(long ownerHandle, ComWrapper<ICamApiViewPort> viewPortCom)
+    {
+        var thread = new Thread(() =>
+        {
+            try
+            {
+                var window = new ViewControlWindow(viewPortCom);
+                var helper = new System.Windows.Interop.WindowInteropHelper(window);
+                helper.Owner = (IntPtr)ownerHandle;
+                window.Closed += (s, e) => 
+                {
+                    window.Dispose();
+                    _canUnload = true;
+                };
+                window.Show();
+                System.Windows.Threading.Dispatcher.Run();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        });
+
+        thread.SetApartmentState(ApartmentState.STA);
+        thread.Start();
+    }
+    
+    /// <inheritdoc />
+    public void Run(IExtensionUtilityContext context, out TResultStatus resultStatus)
+    {
+        resultStatus = default;
+        try
+        {
+            // arrange
+            using var applicationCom = ComWrapper.Create(context.CamApplication);
+            using var applicationMainFormCom = applicationCom.MainForm();
+            _mainWindowHandle = applicationMainFormCom.Invoke(mainForm => mainForm.MainWindowHandle);
+            var viewPortCom = applicationMainFormCom.InvokeAndWrap(mainForm => mainForm.MainViewPort);
+
+            // show form
+            try
+            {
+                ShowForm(_mainWindowHandle, viewPortCom);
+            }
+            catch (Exception)
+            {
+                viewPortCom.Dispose();
+                throw;
+            }
+        }
+        catch (Exception e)
+        {
+            resultStatus.Code = TResultStatusCode.rsError;
+            resultStatus.Description = e.Message;
+        }
+    }
+
+    /// <summary>
+    /// Allow unloading only when a window is closed
+    /// </summary>
+    public bool CanUnload
+    {
+        get => _canUnload;
+        set
+        {
+            
+        }
+    }
+}
---
+++ UI/ExtensionManageViewNet/project/main/ViewControlWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using CAMAPI.DotnetHelper;
+using CAMAPI.ViewCube;
+using CAMAPI.ViewPort;
+
+namespace ExtensionManageViewNet;
+
+/// <summary>
+/// Interaction logic for ViewControlWindow.xaml
+/// </summary>
+public partial class ViewControlWindow : Window, IDisposable
+{
+    private readonly ComWrapper<ICamApiViewPort> _viewPortCom;
+
+    /// <summary>
+    /// Interaction logic for ViewControlWindow.xaml
+    /// </summary>
+    public ViewControlWindow(ComWrapper<ICamApiViewPort> viewPortCom)
+    {
+        _viewPortCom = viewPortCom;
+        InitializeComponent();
+    }
+
+    private void Face_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && button.Tag is string tagStr)
+        {
+            if (Enum.TryParse<TViewCubeRotateMode>(tagStr, out var mode))
+            {
+                try
+                {
+                    _viewPortCom.ZoomAll(true);
+                    using var cubeCom = _viewPortCom.GetCube();
+                    cubeCom.Rotate(mode);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Dispose COM wrappers
+    /// </summary>
+    public void Dispose()
+    {
+        _viewPortCom.Dispose();
+    }
+}
