05eaefef3b7d1c6c0a4348b872334b969219b7f7
igrek <igrek@encycam.com>
2026-02-11 14:11:19 +0200

    New example - adding user operations

---
+++ Technologist/Operation/CreateUserOperationNet/project/main/ExtensionCreateUserOperation.cs
@@ -0,0 +1,51 @@
+﻿using CAMAPI.Application;
+using CAMAPI.DotnetHelper;
+using CAMAPI.Extensions;
+using CAMAPI.ResultStatus;
+
+namespace CreateUserOperationNet;
+
+/// <summary>
+/// Extension to create a user operation in the current project
+/// </summary>
+public class ExtensionCreateUserOperation : IExtension, IExtensionUtility
+{
+    /// <inheritdoc />
+    public IExtensionInfo? Info { get; set; }
+
+    /// <summary>
+    /// Extension to create a user operation in the current project
+    /// </summary>
+    /// <param name="context">Information about the current running instance</param>
+    /// <param name="resultStatus">Structure to return error</param>
+    public void Run(IExtensionUtilityContext context, out TResultStatus resultStatus)
+    {
+        resultStatus = default;
+        
+        try
+        {
+            // create a user operation
+            using var applicationCom = ComWrapper.Create(context.CamApplication);
+            using var projectCom = applicationCom.GetActiveProject();
+            using var technologistCom = projectCom.Technologist();
+            using var userOperationsCom = applicationCom.UserTechOperationList();
+            using var currentOperationCom = technologistCom.CurrentOperation();
+            var currentOperationId = currentOperationCom.Id();
+            using var infoCom = userOperationsCom.AddFromOp(currentOperationCom.Name() + " NEW", currentOperationCom);
+            var userOperationGuid = infoCom.GUID();
+            
+            // add to technology
+            technologistCom.Invoke(technologist =>
+            {
+                technologist.CreateOperationFromUserTemplate(userOperationGuid, currentOperationId, out var ret);
+                if (ret.Code == TResultStatusCode.rsError)
+                    throw new Exception(ret.Description);
+            });
+        }
+        catch (Exception e)
+        {
+            resultStatus.Code = TResultStatusCode.rsError;
+            resultStatus.Description = e.Message;
+        }
+    }
+}
---
+++ Technologist/Operation/CreateUserOperationNet/project/main/README.md
@@ -0,0 +1,10 @@
+# RenameOperationsNet usage example
+
+This example demonstrates how to create operation from user template.
+
+# How to build
+
+1. Compile this project with **"./commands/build.cmd"** or menu **"Terminal/Run build task"** in VSCode.
+2. Install the extension in the CAM system settings window, Extensions tab by specifying the file **bin/Debug/CreateUserOperationNet.settings.json**.
+3. Restart ENCY.
+4. In the utilities menu, select the "**Create user operation**" item. All operation names will be numbered, including child elements.
